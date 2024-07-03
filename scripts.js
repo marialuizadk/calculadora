4c3524ac3e4d288ac8693f2b350c98bc174397eb
maria luiza <125272614+marialuizadk@users.noreply.github.com>
2024-07-02 21:33:21 -0300

    444

--- scripts.js
+++ scripts.js
@@ -1,83 +1,66 @@
-/*-----------------------------------------CALCULADORA----------------------------------------------------------*/
-// pego os elementos li e div input
-var btn 	  = document.querySelectorAll(".key li"),
-	input 	  = document.querySelector(".input-valor"),
-	operador  = document.querySelectorAll(".operador");
+// Script Calculadora
 
-// Percorro o array para usar todas as informações
-for(var i = 0; i < btn.length; i++){
-	document.onkeypress = function(){
-		var key = event.keyCode;
-		//console.log(key);
-		for(var e = 0; e <= 10; e++){
-			if(key === (48+e)){
-				input.innerHTML += e;
-			}
-		}
-		switch (key){
-			case 42:
-				input.innerHTML += "*";
-				break;
-			case 43:
-				input.innerHTML += "+";
-				break;
-			case 45:
-				input.innerHTML += "-";
-				break;
-			case 46:
-				input.innerHTML += ".";
-				break;
-			case 47:
-				input.innerHTML += "/";
-				break;
-			case 13:
-			case 61:
-				var equacao = input.innerHTML;
-				if(equacao){
-					try {
-						input.innerHTML = eval(equacao);
-					} catch (e) {
-						alert('Erro na expressão');
+function operar(tecla){
+    var pantalla= document.getElementsByClassName('modulo');
+    var textoP = pantalla[0].firstChild;
+    switch (tecla) {
+      case 'c':
+        textoP.nodeValue= ' ';
+        break;
+      case '=':
+        resultado = eval(textoP.nodeValue);
+        textoP.nodeValue= resultado;
+        break;
+      default:
+        textoP.nodeValue= textoP.nodeValue+ tecla;
+        break;
+    }
+  }
 
-					} 
-				}
-				break;
-			case 67:
-			case 99:
-				input.innerHTML = "";
-				break;						
-			default:
-				//console.log("Procurando erros?");
-				break;
-		}
-	};
-	btn[i].addEventListener('click',function(){
-		var btnVal 	 = this.innerHTML,
-			inputVal = input.innerHTML;
-		//console.log(btnVal);
+  // Cambio de color
 
-		// crio um clear caso o c for clicado
-		switch (btnVal){
-			case "c":
-				input.innerHTML = "";
-				break;
-			case "=":
-				// Crio a variável de equação aqui eu utilizo a função eval do javascript
-				var equacao = inputVal;
-				
-				if(equacao){
-					try {
-						input.innerHTML = eval(equacao);
-					} catch (e) {
-						alert('Erro na expressão');
+var r = $('.ripple'),
+  p = $('.pantalla');
 
-					} 
-				}
-				break;
-			default:
-				input.innerHTML += btnVal;
-				break;	
-		}
 
-	});
+function paleta(){
+$('.paleta').css('display','flex');
+$('.paleta').animate({
+  width: '70%'
+},300);
+rippleOut();
+}
+
+function paletaOut(){
+$('.paleta').animate({
+  width: '0%'
+},300);
+setTimeout(function(){
+  $('.paleta').css('display','none');
+},300);
+}
+
+function ripple(){
+$('.ripple').animate({
+  width: '1000px',
+  height: '1000px',
+  top: '-200px',
+  left: '-200px'
+  },400);
+}
+
+function rippleOut(){
+r.css('width','0px');
+r.css('height','0px');
+r.css('top','-5px');
+r.css('left','-5px');
+}
+
+function cambioColor(e){
+r.css('background',e);
+ripple();
+paletaOut();
+setTimeout(function(){
+  p.css('background',e);
+},400);
 }
